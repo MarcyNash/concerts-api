29c324804fd68eed10d0404e081ea0f07ccb285e
MarcyNash <marcywsa@gmail.com>
2017-07-23 21:13:22 -0400

    Added new concert csv files for seeding the Heroku db

--- csv/concerts1.sh
+++ csv/concerts1.sh
@@ -1,4 +1,4 @@
-\copy concerts(concert_name,concert_date,concert_time,artist, venue,venue_address,cost) FROM 'csv/concerts1.csv' WITH (FORMAT csv, HEADER true)
+\copy concerts(concert_name,concert_date,concert_time,artist, venue,venue_address,cost,created_at,updated_at) FROM 'csv/concerts3.csv' WITH (FORMAT csv, HEADER true)
 
 
 bin/rails generate scaffold myconcerts profile:references concert:references
